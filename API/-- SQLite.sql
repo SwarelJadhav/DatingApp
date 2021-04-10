e4b05b43fa181ef6ced38299f5b25b13e52b7dd9
Swarel Jadhav <swarel.jadhav27@gmail.com>
2021-04-10 15:36:25 +0530

    initial commit

---
+++ API/-- SQLite.sql
@@ -0,0 +1,12 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1,"Swarel");
+
+INSERT INTO Users (Id, UserName)
+VALUES (2,"Sagar");
+
+INSERT INTO Users (Id, UserName)
+VALUES (3,"Avadhut");
+
+INSERT INTO Users (Id, UserName)
+VALUES (4,"Suraj");
